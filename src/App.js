9ca9efd6b470d79c846f55f5d7699379be0b9599
Sravya N <109574420+snagalak@users.noreply.github.com>
2023-07-15 12:15:46 +0400

    auth0 integration

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+import LoginButton from './components/LoginButton';
+
+
+function App() {
+  return (
+    <LoginButton />
+  );
+}
+
+export default App;
